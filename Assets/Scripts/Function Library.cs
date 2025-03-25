550ad4155fd5ea7e3c92c2e2abc1596f23852fb7
Erin <erin.klarisa@gmail.com>
2025-03-25 23:05:47 +0800

    z axis

--- Assets/Scripts/Function Library.cs
+++ Assets/Scripts/Function Library.cs
@@ -4,7 +4,7 @@ using static UnityEngine.Mathf;
 public static class FunctionLibrary
 {    //directory
 
-    public delegate float Function(float x, float t);
+    public delegate float Function(float x, float z, float t);
     public enum FunctionName { Wave, MultiWave, Ripple }
 
     static Function[] functions = { Wave, MultiWave, Ripple };
@@ -16,22 +16,23 @@ public static class FunctionLibrary
 
 
 
-    public static float Wave(float x, float t)
+    public static float Wave(float x, float z, float t)
     {
-        return Sin(PI * (x + t));
+        return Sin(PI * (x +z+ t));
     }
 
-    public static float MultiWave (float x,float t)
+    public static float MultiWave (float x, float z, float t)
     {
         float y =  Sin(PI * (x+ 0.5f * t));
-        y += 0.5f * Sin(2f * PI * (x + t));
+        y += 0.5f * Sin(2f * PI * (z + t));
+        y += Sin(PI * (x + z + 0.25f * t));
         //2pi -> sin wavelength is 720 but pi * x -> frequency so 2pix also means it goes 2 times faster. we're also told to cut the amp by half
-        return y * (2f/3f) ;
+        return y * (1f/2.5f) ;
     }
 
-    public static float Ripple (float x, float t)
+    public static float Ripple (float x, float z, float t)
     {
-        float d = Abs(x);
+        float d = Sqrt(x * x + z * z);
         float y = Sin(PI * (4f * d - t));
         return y/(1f +10f *d);
     }
--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -25,12 +25,18 @@ public class Graph:MonoBehaviour
         var position = Vector3.zero;
         var scale = Vector3.one * block_space;
 
-        points = new Transform[resolution];
+        points = new Transform[resolution * resolution];
 
-        for (int i = 0; i < points.Length; i++)
+        for (int i = 0, x = 0, z=0; i < points.Length; i++, x++)
         {
+            if (x == resolution)
+            {
+                x = 0;
+                z += 1;
+            }
             Transform point = points[i] = Instantiate(pointPrefab);
-            position.x = (i + 0.5f) * block_space - 1f;
+            position.x = (x + 0.5f) * block_space - 1f;
+            position.z = (z + 0.5f) * block_space - 1f;
             point.localPosition = position;
             point.localScale = scale;
             point.SetParent(transform, false);
@@ -46,7 +52,7 @@ public class Graph:MonoBehaviour
         {
             Transform point = points[i];
             Vector3 position = point.localPosition;
-            position.y = f(position.x, time);
+            position.y = f(position.x, position.z, time);
            
             //sin(pi*(x+t))
             point.localPosition = position;
